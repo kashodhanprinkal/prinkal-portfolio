9c6188441bb46ac4e7306e855d5b543e6eb398f2
unknown <kashodhanprinkal@gmail.com>
2026-08-01 00:11:16 +0530

    seo fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,30 @@
-
 import "./globals.css";
 import CustomCursor from "@/components/ui/CustomCursor";
 import Preloader from "@/components/ui/Preloader";
-import  { Caveat } from "next/font/google"
+import { Caveat } from "next/font/google";
 
 export const metadata = {
-   title: "Prinkal Kashodhan |Software Developer",
+  title: "Prinkal Kashodhan | Full Stack Software Developer",
   description:
-    "Portfolio of Prinkal Kashodhan - Frontend Developer specializing in React, Next.js, JavaScript, and modern web development.",
+    "Prinkal Kashodhan — Full Stack Software Developer building fast, scalable web applications with React, Next.js, Node.js, and modern JavaScript. Explore projects, skills, and experience in full stack development.",
+  verification: {
+    google: "Z4T7kg-qTEAYobLC4B14y0NLSbKHaemDgn99vWfMLiQ",
+  },
 };
 
 const caveat = Caveat({
-  subsets:["latin"],
-  weight:["500","600","700"],
-  variable:"--font-caveat"
-})
+  subsets: ["latin"],
+  weight: ["500", "600", "700"],
+  variable: "--font-caveat",
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={caveat.variable}>
-
       <body>
-        <Preloader /> 
+        <Preloader />
         <CustomCursor />
         {children}
-        
       </body>
     </html>
   );
